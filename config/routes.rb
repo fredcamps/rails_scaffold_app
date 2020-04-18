935d36f5917c93da7a45294ca386bd54e3596882
Fred Campos <mail2frede@gmail.com>
2020-04-18 19:33:34 -0300

    add routes for controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace 'api' do
+    namespace 'v1' do
+      get 'healthcheck', action: 'index', controller: 'healthcheck'
+      get 'dns_records/:page', action: 'list', controller: 'dns_records'
+      post 'dns_records', action: 'create', controller: 'dns_records'
+    end
+  end
 end
